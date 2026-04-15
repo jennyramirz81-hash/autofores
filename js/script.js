d18be5afbd02e5542241f0b5de43d591d5e1921e
jennyramirz81-hash <jennyramirz81@gmail.com>
2026-04-15 15:42:09 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
-document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("year").textContent = new Date().getFullYear();
 
-const form = document.getElementById("whatsappForm");
-
-form.addEventListener("submit", function(e) {
+document.getElementById("whatsappForm").addEventListener("submit", function(e) {
   e.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const phone = document.getElementById("phone").value;
-  const vehicle = document.getElementById("vehicle").value;
-  const service = document.getElementById("service").value;
-  const message = document.getElementById("message").value;
+  const text = `
+Nombre: ${name.value}
+Teléfono: ${phone.value}
+Vehículo: ${vehicle.value}
+Servicio: ${service.value}
+Mensaje: ${message.value}
+`;
 
-  const text = `Hola, soy ${name}
-Teléfono: ${phone}
-Vehículo: ${vehicle}
-Servicio: ${service}
-Mensaje: ${message}`;
-
-  const url = `https://wa.me/573142371493?text=${encodeURIComponent(text)}`;
-  window.open(url, "_blank");
+  window.open(`https://wa.me/573142371493?text=${encodeURIComponent(text)}`);
 });
